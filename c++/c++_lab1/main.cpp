6077859811f947eadc9061766fc6e44d4553e06f
lazaroofarrill <lazaroofarrill@gmail.com>
2022-01-07 21:16:29 -0500

    lol

--- c++/c++_lab1/main.cpp
+++ c++/c++_lab1/main.cpp
@@ -1,7 +1,11 @@
+#include <cMetaInterval.h>
 #include "pandaFramework.h"
 #include "pandaSystem.h"
 #include "genericAsyncTask.h"
 #include "asyncTaskManager.h"
+
+#include "cIntervalManager.h"
+#include "cLerpNodePathInterval.h"
 
 // The global task manager
 PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr();
@@ -40,7 +44,61 @@ int main(int argc, char *argv[]) {
     scene.set_scale(0.25f, 0.25f, 0.25f);
     scene.set_pos(-8, 42, 0);
 
+    NodePath pandaActor = window->load_model(framework.get_models(), "models/panda-model");
+    pandaActor.set_scale(0.005);
+    pandaActor.reparent_to(window->get_render());
+
+    window->load_model(pandaActor, "models/panda-walk4");
+    window->loop_animations(0);
+
+    // Create the lerp intervals needed to walk back and forth
+    PT(CLerpNodePathInterval) pandaPosInterval1, pandaPosInterval2,
+            pandaHprInterval1, pandaHprInterval2;
+    pandaPosInterval1 = new CLerpNodePathInterval("pandaPosInterval1",
+                                                  13.0, CLerpInterval::BT_no_blend,
+                                                  true, false, pandaActor, NodePath());
+    pandaPosInterval1->set_start_pos(LPoint3(0, 10, 0));
+    pandaPosInterval1->set_end_pos(LPoint3(0, -10, 0));
+
+    pandaPosInterval2 = new CLerpNodePathInterval("pandaPosInterval2",
+                                                  13.0, CLerpInterval::BT_no_blend,
+                                                  true, false, pandaActor, NodePath());
+    pandaPosInterval2->set_start_pos(LPoint3(0, -10, 0));
+    pandaPosInterval2->set_end_pos(LPoint3(0, 10, 0));
+
+    pandaHprInterval1 = new CLerpNodePathInterval("pandaHprInterval1", 3.0,
+                                                  CLerpInterval::BT_no_blend,
+                                                  true, false, pandaActor, NodePath());
+    pandaHprInterval1->set_start_hpr(LPoint3(0, 0, 0));
+    pandaHprInterval1->set_end_hpr(LPoint3(180, 0, 0));
+
+    pandaHprInterval2 = new CLerpNodePathInterval("pandaHprInterval2", 3.0,
+                                                  CLerpInterval::BT_no_blend,
+                                                  true, false, pandaActor, NodePath());
+    pandaHprInterval2->set_start_hpr(LPoint3(180, 0, 0));
+    pandaHprInterval2->set_end_hpr(LPoint3(0, 0, 0));
+
+    // Create and play the sequence that coordinates the intervals
+    PT(CMetaInterval) pandaPace;
+    pandaPace = new CMetaInterval("pandaPace");
+    pandaPace->add_c_interval(pandaPosInterval1, 0,
+                              CMetaInterval::RS_previous_end);
+    pandaPace->add_c_interval(pandaHprInterval1, 0,
+                              CMetaInterval::RS_previous_end);
+    pandaPace->add_c_interval(pandaPosInterval2, 0,
+                              CMetaInterval::RS_previous_end);
+    pandaPace->add_c_interval(pandaHprInterval2, 0,
+                              CMetaInterval::RS_previous_end);
+    pandaPace->loop();
+
+
+    //setup task
     taskMgr->add(new GenericAsyncTask("Spins the camera", &spinCameraTask, nullptr));
+
+    Thread *current_thread = Thread::get_current_thread();
+    while (framework.do_frame(current_thread)) {
+        CIntervalManager::get_global_ptr()->step();
+    }
 
 
     // Do the main loop, equal to run() in python
